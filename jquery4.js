f28e7d8767620cb88695cb3326f02c70e643baf2
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-07 23:48:24 +0000

    jquery version4

---
+++ jquery4.js
@@ -0,0 +1,19 @@
+// work i have done in the console on a randomn website 
+
+
+$("#EmpHeroAndEmpInfo");
+n.fn.init [div#EmpHeroAndEmpInfo, context: document, selector: "#EmpHeroAndEmpInfo"]0: div#EmpHeroAndEmpInfocontext: documentlength: 1selector: "#EmpHeroAndEmpInfo"__proto__: Object(0)
+var heading = $("#EmpHeroAndEmpInfo");
+undefined
+heading.css({position: "relative"});
+n.fn.init [div#EmpHeroAndEmpInfo, context: document, selector: "#EmpHeroAndEmpInfo"]0: div#EmpHeroAndEmpInfocontext: documentlength: 1selector: "#EmpHeroAndEmpInfo"__proto__: Object(0)
+heading.animate({left:100});
+n.fn.init [div#EmpHeroAndEmpInfo, context: document, selector: "#EmpHeroAndEmpInfo"]
+heading.animate({left:100});
+n.fn.init [div#EmpHeroAndEmpInfo, context: document, selector: "#EmpHeroAndEmpInfo"]
+heading[0]
+<div id=​"EmpHeroAndEmpInfo" style=​"position:​ relative;​ left:​ 100px;​">​…​</div>​
+heading[0].animate({left: 20})
+VM286230:1 Invalid keyframe value for property left: 20
+(anonymous) @ VM286230:1
+Animation {startTime: null, currentTime: 0, playbackRate: 1, playState: "pending", id: "", …}
