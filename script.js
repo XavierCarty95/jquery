6885810a7d5bb1654be0f65b76f484ef400702b6
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-08 01:14:10 +0000

    traversing the DOM

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
-// // $("header nav li:first").css({border: "2px solid red"})
-// $("header nav li: last").css({border: "2px solid red"})
-// $("#contact ul:first-child").css({border: "2px solid red"});
-// $("#contact ul:last-child").css({border: "2px solid red"});
+// // $('#contact-methods').next().css({border: "3px solid red"});
+// $("#social-nav").prev().css({border: "3px solid pink"});
+// $(".banner-title").parent().css({border: "3px solid pink"})
+// $("#social-nav").children().css({border: "3px solid green"})
 
-// $("header nav li:even").css({border: "2px solid blue"})
-// $("header nav li:odd").css({border: "2px solid yellow"})
+// $("#contact").find(".facebook").css({border: " 3px solid green "});
 
-// $("section:not('#contact')").css({border: "2px solid green"});
-// $("#social-nav li:li(3)").css({border: "2px solid blue"})
-// $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+$("#social-nav").closest(".wrapper").css({border:"3px solid orange"})
 
-// $("img[alt]").css({border: "2px solid pink"})
-   $("img[alt=quote]").css({border: "3px solid purple"})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // $("header nav li:first").css({border: "2px solid red"})
+// // $("header nav li: last").css({border: "2px solid red"})
+// // $("#contact ul:first-child").css({border: "2px solid red"});
+// // $("#contact ul:last-child").css({border: "2px solid red"});
+
+// // $("header nav li:even").css({border: "2px solid blue"})
+// // $("header nav li:odd").css({border: "2px solid yellow"})
+
+// // $("section:not('#contact')").css({border: "2px solid green"});
+// // $("#social-nav li:li(3)").css({border: "2px solid blue"})
+// // $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+
+// // $("img[alt]").css({border: "2px solid pink"})
+//   $("img[alt=quote]").css({border: "3px solid purple"})
