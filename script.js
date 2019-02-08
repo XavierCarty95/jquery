548caa20fcf0d93cd98ba2dbb3e7e9a851e8ab78
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-08 01:38:08 +0000

    add content in jquery

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-$("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"}).closet("section").css({border: "2px solid blue"});
 
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'> The big fight live: Rams vs Cheese!</div>"
 
-
-// // // $('#contact-methods').next().css({border: "3px solid red"});
-// // $("#social-nav").prev().css({border: "3px solid pink"});
-// // $(".banner-title").parent().css({border: "3px solid pink"})
-// // $("#social-nav").children().css({border: "3px solid green"})
-
-// // $("#contact").find(".facebook").css({border: " 3px solid green "});
-
-// $("#social-nav").closest(".wrapper").css({border:"3px solid orange"})
+// $("#tweets div p").before(tweet)
+// $("#tweets div").append(tweet)
+// $("#tweets div p").after(tweet)
+// $("#tweets div p").html(tweet)
 
 
 
 
 
 
+// // $("#contact-methods").css({border: "2px solid red"}).next().css
 
 
 
@@ -25,6 +21,18 @@ $("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px so
 
 
 
+// ({border: "2px solid green"}).closet("section").css({border: "2px solid blue"});
+
+
+
+// // // // $('#contact-methods').next().css({border: "3px solid red"});
+// // // $("#social-nav").prev().css({border: "3px solid pink"});
+// // // $(".banner-title").parent().css({border: "3px solid pink"})
+// // // $("#social-nav").children().css({border: "3px solid green"})
+
+// // // $("#contact").find(".facebook").css({border: " 3px solid green "});
+
+// // $("#social-nav").closest(".wrapper").css({border:"3px solid orange"})
 
 
 
@@ -38,17 +46,32 @@ $("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px so
 
 
 
-// // // $("header nav li:first").css({border: "2px solid red"})
-// // $("header nav li: last").css({border: "2px solid red"})
-// // $("#contact ul:first-child").css({border: "2px solid red"});
-// // $("#contact ul:last-child").css({border: "2px solid red"});
 
-// // $("header nav li:even").css({border: "2px solid blue"})
-// // $("header nav li:odd").css({border: "2px solid yellow"})
 
-// // $("section:not('#contact')").css({border: "2px solid green"});
-// // $("#social-nav li:li(3)").css({border: "2px solid blue"})
-// // $("#social-nav li:gt(2)").css({border: "2px solid blue"});
 
-// // $("img[alt]").css({border: "2px solid pink"})
-//   $("img[alt=quote]").css({border: "3px solid purple"})
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // $("header nav li:first").css({border: "2px solid red"})
+// // // $("header nav li: last").css({border: "2px solid red"})
+// // // $("#contact ul:first-child").css({border: "2px solid red"});
+// // // $("#contact ul:last-child").css({border: "2px solid red"});
+
+// // // $("header nav li:even").css({border: "2px solid blue"})
+// // // $("header nav li:odd").css({border: "2px solid yellow"})
+
+// // // $("section:not('#contact')").css({border: "2px solid green"});
+// // // $("#social-nav li:li(3)").css({border: "2px solid blue"})
+// // // $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+
+// // // $("img[alt]").css({border: "2px solid pink"})
+// //   $("img[alt=quote]").css({border: "3px solid purple"})
