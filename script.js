5fe3746c9a7d18ecc42468aab571c70cd3aff46c
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-08 00:53:22 +0000

    jquery filters

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-$("h3").css({border: "3px solid blue"});
-$(".wrapper").css({border:"3px solid red"});
-$("#clients").css({border: "3px solid yellow"})
+// // $("header nav li:first").css({border: "2px solid red"})
+// $("header nav li: last").css({border: "2px solid red"})
+// $("#contact ul:first-child").css({border: "2px solid red"});
+// $("#contact ul:last-child").css({border: "2px solid red"});
+
+// $("header nav li:even").css({border: "2px solid blue"})
+// $("header nav li:odd").css({border: "2px solid yellow"})
+
+// $("section:not('#contact')").css({border: "2px solid green"});
+// $("#social-nav li:li(3)").css({border: "2px solid blue"})
+// $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+
+// $("img[alt]").css({border: "2px solid pink"})
+   $("img[alt=quote]").css({border: "3px solid purple"})
