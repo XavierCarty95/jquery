4a6d151349772e8323b1d7fd2a73530885812687
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-09 17:27:24 +0000

    removing and installing classes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var button = $("#lead-banner a");
 
-button[0].onclick = function(){
-    $("#points-of-sale").toggleClass("open");
-    return false;
-}
+
+
+
+
+
+
+
+
+
+// var button = $("#lead-banner a");
+
+// button[0].onclick = function(){
+//     $("#points-of-sale").toggleClass("open");
+//     return false;
+// }
 
 
 
