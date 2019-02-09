717f44f2c87295282dc625d95aed91e9f980dd55
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-09 19:39:12 +0000

    hide and show elements

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
 $(document).ready(function(){
-     $("section > h2").on("click" , function(){
-      $(this).fadeTo(200, 0.2)
-             .fadeTo(200, 0.8)
-             .fadeTo(200, 0.2)
-             .fadeTo(200, 0.8)
-             .fadeTo(200, 0.2)
-             .fadeTo(200, 0.8);
-             
-     })
-     
-})
+   $("img[alt=map]").on("click" , function(){
+       $("section > h2").toggle(1000);
+   });
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 // $(document).ready(function(){
-//     $("section > h2").on("click" , function(){
-//         $(this).fadeOut(2000).fadeIn(5000)
-//     })
+//      $("section > h2").on("click" , function(){
+//       $(this).fadeTo(200, 0.2)
+//              .fadeTo(200, 0.8)
+//              .fadeTo(200, 0.2)
+//              .fadeTo(200, 0.8)
+//              .fadeTo(200, 0.2)
+//              .fadeTo(200, 0.8);
+             
+//      })
+     
 // })
+
+
+
+
+// // $(document).ready(function(){
+// //     $("section > h2").on("click" , function(){
+// //         $(this).fadeOut(2000).fadeIn(5000)
+// //     })
+// // })
 
 
 
          
 
 
-// $(document).ready(function(){
+// // $(document).ready(function(){
     
-//     function complete(){
-//             alert("Animation complete")
-//     }
+// //     function complete(){
+// //             alert("Animation complete")
+// //     }
     
-//     $("section > h2").on("click", function(){
+// //     $("section > h2").on("click", function(){
         
-//         $(this).animate({"left": "200px", "width" : "500px", "height" : "100px"}, 1000, "linear", complete)
-//         })
+// //         $(this).animate({"left": "200px", "width" : "500px", "height" : "100px"}, 1000, "linear", complete)
+// //         })
         
    
-// });
+// // });
 
 
 
@@ -47,16 +71,16 @@ $(document).ready(function(){
 
 
 
-// $(document).ready(function(){
-//     $("*").on("click",function(e){
+// // $(document).ready(function(){
+// //     $("*").on("click",function(e){
         
-//         console.log(e.target)
-//         console.log("The event type is: " + e.type)
-//         console.log("x co-ordinate of the event " + e.pageX);
-//         console.log("x co-ordinate of the event " + e.pageY)
-//         e.stopPropagation()
-//     })
-// })
+// //         console.log(e.target)
+// //         console.log("The event type is: " + e.type)
+// //         console.log("x co-ordinate of the event " + e.pageX);
+// //         console.log("x co-ordinate of the event " + e.pageY)
+// //         e.stopPropagation()
+// //     })
+// // })
 
 
 
@@ -68,22 +92,22 @@ $(document).ready(function(){
 
 
 
-// $(function(){
+// // $(function(){
     
     
     
-// })
+// // })
 
-// $(window).load(function(){
+// // $(window).load(function(){
     
     
-// })
+// // })
 
 
-// $("#lead-banner").dblclick(function(){
-//     alert("you click me");
+// // $("#lead-banner").dblclick(function(){
+// //     alert("you click me");
     
-// })
+// // })
 
 
 
@@ -91,71 +115,25 @@ $(document).ready(function(){
 
 
 
-// var myLis = $("#points-of-sale li");
+// // var myLis = $("#points-of-sale li");
 
-// myLis.on("click", function(){
+// // myLis.on("click", function(){
     
-//     $(this).css({"background" : "pink"})
+// //     $(this).css({"background" : "pink"})
     
-//     myLis.off("click")
+// //     myLis.off("click")
     
     
-// })
+// // })
 
 
 
-// $("#social-nav").css({
-//     "top": "-400px",
-//     "left":"1500px",
-//     "opacity": "0.5",
-//     "border-top": "4px solid red"
-// });
-
-
-
-
-
-
-
-
-
-
-
-// var button = $("#lead-banner a");
-
-// button[0].onclick = function(){
-//     $("#points-of-sale").toggleClass("open");
-//     return false;
-// }
-
-
-
-
-
-
-
-
-// $("header .wrapper").removeClass("wrapper");
-// $("header > div").addClass("wrapper");
-
-
-
-
-
-
-// $("#social-nav").css({
-//     "top": "-400px",
-//     "left":"1500px",
-//     "opacity": "0.5",
-//     "border-top": "4px solid red"
-// })
-
-
-
-
-// $(".button").empty();
-// $("#point-of-sale").empty()
-// $("#contact img").remove()
+// // $("#social-nav").css({
+// //     "top": "-400px",
+// //     "left":"1500px",
+// //     "opacity": "0.5",
+// //     "border-top": "4px solid red"
+// // });
 
 
 
@@ -165,22 +143,68 @@ $(document).ready(function(){
 
 
 
-// var wrapper ="<div class = 'wrapper'>"
-// var button = $(".button");
-// var wrapped = true
 
-// button[0].onclick = function(){
-//     if(wrapped){
-//         $("section").unwrap();
-//         wrapped = false;
-//         button.text("Wrap");
-//     } else { 
-//         $("section").wrapAll(wrapper);
-//         wrapped = true;
-//         button.text("UnWrap");
+
+// // var button = $("#lead-banner a");
+
+// // button[0].onclick = function(){
+// //     $("#points-of-sale").toggleClass("open");
+// //     return false;
+// // }
+
+
+
+
+
+
+
+
+// // $("header .wrapper").removeClass("wrapper");
+// // $("header > div").addClass("wrapper");
+
+
+
+
+
+
+// // $("#social-nav").css({
+// //     "top": "-400px",
+// //     "left":"1500px",
+// //     "opacity": "0.5",
+// //     "border-top": "4px solid red"
+// // })
+
+
+
+
+// // $(".button").empty();
+// // $("#point-of-sale").empty()
+// // $("#contact img").remove()
+
+
+
+
+
+
+
+
+
+// // var wrapper ="<div class = 'wrapper'>"
+// // var button = $(".button");
+// // var wrapped = true
+
+// // button[0].onclick = function(){
+// //     if(wrapped){
+// //         $("section").unwrap();
+// //         wrapped = false;
+// //         button.text("Wrap");
+// //     } else { 
+// //         $("section").wrapAll(wrapper);
+// //         wrapped = true;
+// //         button.text("UnWrap");
         
-//     }
-// }
+// //     }
+// // }
 
 
 
@@ -188,44 +212,23 @@ $(document).ready(function(){
 
 
 
-// $("section").wrap("<div>")
-// $("section").unwrap()
-// $("section").wrapAll("<div>")
+// // $("section").wrap("<div>")
+// // $("section").unwrap()
+// // $("section").wrapAll("<div>")
 
-// var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'> The big fight live: Rams vs Cheese!</div>"
+// // var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'> The big fight live: Rams vs Cheese!</div>"
 
-// // $("#tweets div p").before(tweet)
-// // $("#tweets div").append(tweet)
-// // $("#tweets div p").after(tweet)
-// // $("#tweets div p").html(tweet)
-
-
-
-
-
-
-// // $("#contact-methods").css({border: "2px solid red"}).next().css
+// // // $("#tweets div p").before(tweet)
+// // // $("#tweets div").append(tweet)
+// // // $("#tweets div p").after(tweet)
+// // // $("#tweets div p").html(tweet)
 
 
 
 
 
 
-
-
-
-// ({border: "2px solid green"}).closet("section").css({border: "2px solid blue"});
-
-
-
-// // // // $('#contact-methods').next().css({border: "3px solid red"});
-// // // $("#social-nav").prev().css({border: "3px solid pink"});
-// // // $(".banner-title").parent().css({border: "3px solid pink"})
-// // // $("#social-nav").children().css({border: "3px solid green"})
-
-// // // $("#contact").find(".facebook").css({border: " 3px solid green "});
-
-// // $("#social-nav").closest(".wrapper").css({border:"3px solid orange"})
+// // // $("#contact-methods").css({border: "2px solid red"}).next().css
 
 
 
@@ -234,6 +237,19 @@ $(document).ready(function(){
 
 
 
+
+// // ({border: "2px solid green"}).closet("section").css({border: "2px solid blue"});
+
+
+
+// // // // // $('#contact-methods').next().css({border: "3px solid red"});
+// // // // $("#social-nav").prev().css({border: "3px solid pink"});
+// // // // $(".banner-title").parent().css({border: "3px solid pink"})
+// // // // $("#social-nav").children().css({border: "3px solid green"})
+
+// // // // $("#contact").find(".facebook").css({border: " 3px solid green "});
+
+// // // $("#social-nav").closest(".wrapper").css({border:"3px solid orange"})
 
 
 
@@ -254,17 +270,25 @@ $(document).ready(function(){
 
 
 
-// // // // $("header nav li:first").css({border: "2px solid red"})
-// // // $("header nav li: last").css({border: "2px solid red"})
-// // // $("#contact ul:first-child").css({border: "2px solid red"});
-// // // $("#contact ul:last-child").css({border: "2px solid red"});
 
-// // // $("header nav li:even").css({border: "2px solid blue"})
-// // // $("header nav li:odd").css({border: "2px solid yellow"})
 
-// // // $("section:not('#contact')").css({border: "2px solid green"});
-// // // $("#social-nav li:li(3)").css({border: "2px solid blue"})
-// // // $("#social-nav li:gt(2)").css({border: "2px solid blue"});
 
-// // // $("img[alt]").css({border: "2px solid pink"})
-// //   $("img[alt=quote]").css({border: "3px solid purple"})
+
+
+
+
+
+// // // // // $("header nav li:first").css({border: "2px solid red"})
+// // // // $("header nav li: last").css({border: "2px solid red"})
+// // // // $("#contact ul:first-child").css({border: "2px solid red"});
+// // // // $("#contact ul:last-child").css({border: "2px solid red"});
+
+// // // // $("header nav li:even").css({border: "2px solid blue"})
+// // // // $("header nav li:odd").css({border: "2px solid yellow"})
+
+// // // // $("section:not('#contact')").css({border: "2px solid green"});
+// // // // $("#social-nav li:li(3)").css({border: "2px solid blue"})
+// // // // $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+
+// // // // $("img[alt]").css({border: "2px solid pink"})
+// // //   $("img[alt=quote]").css({border: "3px solid purple"})
