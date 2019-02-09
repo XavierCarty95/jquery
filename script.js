7a2ed2b8b9db928efd8fb244fd414848299bb138
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-09 19:28:04 +0000

    fading elements in and out

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 $(document).ready(function(){
+     $("section > h2").on("click" , function(){
+      $(this).fadeTo(200, 0.2)
+             .fadeTo(200, 0.8)
+             .fadeTo(200, 0.2)
+             .fadeTo(200, 0.8)
+             .fadeTo(200, 0.2)
+             .fadeTo(200, 0.8);
+             
+     })
+     
+})
+
+
+
+
+// $(document).ready(function(){
+//     $("section > h2").on("click" , function(){
+//         $(this).fadeOut(2000).fadeIn(5000)
+//     })
+// })
+
+
+
+         
+
+
+// $(document).ready(function(){
     
-    function complete(){
-            alert("Animation complete")
-    }
+//     function complete(){
+//             alert("Animation complete")
+//     }
     
-    $("section > h2").on("click", function(){
+//     $("section > h2").on("click", function(){
         
-        $(this).animate({"left": "200px", "width" : "500px", "height" : "100px"}, 1000, "linear", complete)
-        })
+//         $(this).animate({"left": "200px", "width" : "500px", "height" : "100px"}, 1000, "linear", complete)
+//         })
         
    
-});
+// });
 
 
 
