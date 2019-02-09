785af90d28b6a5cd9cabaa27ac380873f5805d1d
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-02-09 19:19:16 +0000

    animate

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     
     $("section > h2").on("click", function(){
         
-        $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", complete)
+        $(this).animate({"left": "200px", "width" : "500px", "height" : "100px"}, 1000, "linear", complete)
         })
         
    
